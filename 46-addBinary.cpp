2ffe8786b1edbde22d03073de5106e759ce769af
Ageng <agengpraba@gmail.com>
2024-03-06 09:04:41 +0700

    day 46

---
+++ 46-addBinary.cpp
@@ -0,0 +1,30 @@
+//<7 kyu>
+
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+#include <cstdint>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string add_binary(uint64_t a, uint64_t b) {
+  int sum=a+b;
+  
+  string res;
+  
+  while (sum>0){
+  int sisa=sum%2;
+     res += (sisa == 0) ? '0' : '1';  
+    sum/=2;
+  }
+  reverse(res.begin(),res.end());
+  return res;
+}
